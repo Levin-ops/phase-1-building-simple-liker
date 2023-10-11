c7e84244d61d3628834772973fa938a246bf3ad6
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-12 01:38:06 +0300

    hearts not functioning

--- main.js
+++ main.js
@@ -4,8 +4,44 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likes = document.getElementsByClassName(`like-glyph`);
+
+const event =likes.addEventListener(`click`,()=>{
+  console.log(`heart clicked`)})
+ 
+
+const errorMod = document.querySelector(`modal`);
 
 
+function errorHandling(error) {
+  errorMod.classList.remove(`hidden`);
+  errorMod.textContent = error;
+  setTimeout(()=> {errorMod.classList.add(`hidden`);}, 3000); 
+}
+
+
+likes.forEach(like => {
+  like.addEventListener(`click`,()=>{
+    console.log(`heart clicked`)})
+  })
+
+    errorMod.classList.add('hidden')
+
+    if (like.textContent === EMPTY_HEART){
+      mimicServerCall()
+        .then(()=> {
+          like.value.textContent= FULL_HEART;
+          like.classList.add(`activated-heart`)
+        })
+        .catch(error =>{
+          errorHandling(error);
+        });
+    }else if (like.value.textContent===FULL_HEART){
+      like,innerText= EMPTY_HEART;
+      like.classList.remove(`activated-heart`)
+    }
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
