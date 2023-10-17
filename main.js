05e394e121a3efe6ff04a34b8567ce7ad8d2cd15
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-18 00:49:20 +0300

    Building a like App 2.0

--- main.js
+++ main.js
@@ -3,45 +3,10 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-const likes = document.getElementsByClassName(`like-glyph`);
-
-const event =likes.addEventListener(`click`,()=>{
-  console.log(`heart clicked`)})
- 
-
-const errorMod = document.querySelector(`modal`);
+const errorModal = document.getElementById("modal");
+errorModal.className = "hidden";
 
 
-function errorHandling(error) {
-  errorMod.classList.remove(`hidden`);
-  errorMod.textContent = error;
-  setTimeout(()=> {errorMod.classList.add(`hidden`);}, 3000); 
-}
-
-
-likes.forEach(like => {
-  like.addEventListener(`click`,()=>{
-    console.log(`heart clicked`)})
-  })
-
-    errorMod.classList.add('hidden')
-
-    if (like.textContent === EMPTY_HEART){
-      mimicServerCall()
-        .then(()=> {
-          like.value.textContent= FULL_HEART;
-          like.classList.add(`activated-heart`)
-        })
-        .catch(error =>{
-          errorHandling(error);
-        });
-    }else if (like.value.textContent===FULL_HEART){
-      like,innerText= EMPTY_HEART;
-      like.classList.remove(`activated-heart`)
-    }
-  });
-});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
